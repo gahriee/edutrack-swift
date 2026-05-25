f54208a4c81a469f14611019ab5f8b106f762177
unknown <edgardollentas04@gmail.com>
2026-05-26 01:10:41 +0800

    Configure App Entry Point and Firebase Initialization

--- edutrack/edutrackApp.swift
+++ edutrack/edutrackApp.swift
@@ -1,17 +1,23 @@
-//
-//  edutrackApp.swift
-//  edutrack
-//
-//  Created by Eli on 5/26/26.
-//
-
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct edutrackApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject private var authViewModel = AuthViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authViewModel)
         }
     }
 }
